6d67b0498ac4bd10b105013e7ea645e806eca318
MaryamPazouki <maryam.pazoki@gmail.com>
2024-12-10 23:21:40 +0000

    last check for today

--- sources/ft_exec.c
+++ sources/ft_exec.c
@@ -26,6 +26,10 @@ void ft_exec(t_pipex *data, char **cmd, int is_last) {
         close(data->pipefd[1]);
 
         // Execute the command
+        for (int i = 0; cmd[i] != NULL; i++) {
+            printf("cmd[%d]: %s\n", i, cmd[i]);
+        }
+
         execvp(cmd[0], cmd);
         perror("Command execution failed"); // This only runs if exec fails
         ft_cleanup(data);
