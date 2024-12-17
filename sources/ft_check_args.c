e6d1d1a22604e2d89515f3552dee1d38c4b9de2c
MaryamPazouki <maryam.pazoki@gmail.com>
2024-12-17 23:47:47 +0000

    send norminette

--- sources/ft_check_args.c
+++ sources/ft_check_args.c
@@ -1,4 +1,3 @@
-
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -13,9 +12,11 @@
 
 #include "pipex.h"
 
-void ft_check_args(int argc) {
-    if (argc != 5) {
-        fprintf(stderr, "Usage: ./pipex file1 cmd1 cmd2 file2\n");
-        exit(EXIT_FAILURE);
-    }
+void	ft_check_args(int argc)
+{
+	if (argc != 5)
+	{
+		fprintf(stderr, "Usage: ./pipex file1 cmd1 cmd2 file2\n");
+		exit(EXIT_FAILURE);
+	}
 }
--- sources/ft_check_path.c
+++ sources/ft_check_path.c
@@ -12,64 +12,54 @@
 
 #include "pipex.h"
 
-
-
 size_t safe_strlen(const char *s) {
-    return s ? strlen(s) : 0;
+    if (s)
+        return (strlen(s));
+    return (0);
 }
 
 char *ft_strjoin_double_ptr(const char *s1, char **s2) {
-    if (!s1 || !s2 || !*s2) {
-        return NULL; // Handle NULL input
-    }
+    if (!s1 || !s2 || !*s2)
+        return (NULL);
 
-    size_t len1 = safe_strlen(s1);
-    size_t len2 = safe_strlen(*s2);
+    size_t len1;
+    size_t len2;
+
+    len1 = safe_strlen(s1);
+    len2 = safe_strlen(*s2);
     char *result = malloc(len1 + len2 + 1);
-
-    if (!result) {
-        return NULL; // Handle memory allocation failure
-    }
-
-    // Copy s1 into result
-    if (s1) {
+    if (!result)
+        return (NULL);
+    if (s1)
         strcpy(result, s1);
-    }
-
-    // Concatenate *s2 to result
-    if (*s2) {
+    if (*s2)
         strcpy(result + len1, *s2);
-    }
-
-    return result;
+    return (result);
 }
 
+char *ft_find_path(char **cmd, char **envp) {
+    char **poss_paths;
+    char *cmd_path;
+    int index;
+    char *temp_path;
 
-char	*ft_find_path(char **cmd, char **envp)
-{
-	char	**poss_paths;
-	char	*cmd_path;
-	int		index;
-	char	*temp_path;
-
-	index = 0;
-	while (ft_strnstr(envp[index], "PATH", 4) == 0)
-		index++;
-	poss_paths = ft_split(envp[index] + 5, ':');
-	index = 0;
-	while (poss_paths[index])
-	{
-		temp_path = ft_strjoin(poss_paths[index], "/");
-		cmd_path = ft_strjoin_double_ptr(temp_path, cmd);
-		free(temp_path);
-		if (access(cmd_path, F_OK) == 0)
-			return (cmd_path);
-		free(cmd_path);
-		index++;
-	}
-	index = -1;
-	while (poss_paths[++index])
-		free(poss_paths[index]);
-	free(poss_paths);
-	return (0);
+    index = 0;
+    while (ft_strnstr(envp[index], "PATH", 4) == 0)
+        index++;
+    poss_paths = ft_split(envp[index] + 5, ':');
+    index = 0;
+    while (poss_paths[index]) {
+        temp_path = ft_strjoin(poss_paths[index], "/");
+        cmd_path = ft_strjoin_double_ptr(temp_path, cmd);
+        free(temp_path);
+        if (access(cmd_path, F_OK) == 0)
+            return (cmd_path);
+        free(cmd_path);
+        index++;
+    }
+    index = -1;
+    while (poss_paths[++index])
+        free(poss_paths[index]);
+    free(poss_paths);
+    return (NULL);
 }
